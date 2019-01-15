29dbd631cc2f873bf39dfb001c18d0fd3ac8e118
Daniil Popov <int02h@gmail.com>
2019-01-15 23:21:52 +0300

    demo sample with annotation on method's argument

--- sample/src/main/java/com/dpforge/fastproxy/sample/Foo.java
+++ sample/src/main/java/com/dpforge/fastproxy/sample/Foo.java
@@ -4,4 +4,6 @@ public interface Foo {
     void bar();
 
     void zzz(boolean b, String s, int i);
+
+    void argName(@ArgName("ololo") Object value);
 }
--- sample/src/main/java/com/dpforge/fastproxy/sample/MainActivity.java
+++ sample/src/main/java/com/dpforge/fastproxy/sample/MainActivity.java
@@ -1,14 +1,14 @@
 package com.dpforge.fastproxy.sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.dpforge.fastproxy.ProxyBuilder;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
-import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,10 +20,41 @@ public class MainActivity extends AppCompatActivity {
         Foo foo = ProxyBuilder.createProxy(getApplicationContext(), Foo.class, new InvocationHandler() {
             @Override
             public Object invoke(final Object proxy, final Method method, final Object[] args) {
-                return Log.d("_@_", "Method '" + method + "' called with args: " + Arrays.toString(args));
+                dumpMethodInfo(method, args);
+                return null;
             }
         });
         foo.bar();
-        foo.zzz( true, "Hello World!", -5);
+        foo.zzz(true, "Hello World!", -5);
+        foo.argName(this);
+    }
+
+    private void dumpMethodInfo(final Method method, final Object[] args) {
+        final StringBuilder builder = new StringBuilder();
+        builder.append("Method '").append(method).append("' called with args: [");
+
+        String argSeparator = "";
+        for (final Object arg : args) {
+            final Annotation[][] parameterAnnotations = method.getParameterAnnotations();
+            for (final Annotation[] annotations : parameterAnnotations) {
+                if (annotations.length > 0) {
+                    builder.append('(');
+                    String annotationSeparator = "";
+                    for (Annotation a : annotations) {
+                        if (a instanceof ArgName) {
+                            builder.append(annotationSeparator).append(((ArgName) a).value());
+                            annotationSeparator = " ";
+                        }
+                    }
+                    builder.append(") ");
+                }
+            }
+            builder.append(argSeparator).append(arg);
+            argSeparator = ", ";
+        }
+
+        builder.append(']');
+
+        Log.d("_@_", builder.toString());
     }
 }
